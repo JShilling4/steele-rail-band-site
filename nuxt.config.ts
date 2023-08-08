bbba1a171d7e1bf1f757d19665123246397012f1
Justin Shilling <loveofmusic25@hotmail.com>
2023-08-08 09:31:52 -0400

    add meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,10 +11,42 @@ export default defineNuxtConfig({
         {
           charset: "utf-8",
         },
+        {
+          property: "og:title",
+          content: "Ride The Rail | Steele Rail Band",
+        },
+        {
+          property: "og:url",
+          content: "https://www.steelerailband.com",
+        },
+        {
+          property: "og:site_name",
+          content: "Steele Rail Band",
+        },
+        {
+          property: "og:image",
+          content: "https://www.steelerailband.com/og-image.jpg",
+        },
+        {
+          name: "description",
+          content:
+            "The Steele Rail Band is a variety band that covers music spanning several decades in many different styles - such as country, southern rock, alternative, classic rock and blues.",
+        },
+        {
+          name: "keywords",
+          content:
+            "music, band, country music, rock music, blues, live music near me, dancing",
+        },
       ],
       script: [],
       style: [],
-      link: [],
+      link: [
+        {
+          rel: "canonical",
+          href: "https://www.steelerailband.com",
+        },
+      ],
+      title: "Steele Rail Band | Ride The Rail",
       noscript: [
         // <noscript>Javascript is required</noscript>
         { children: "Javascript is required" },
