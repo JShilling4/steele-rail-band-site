926e6ae2964e0bdaf8d1e6265fd9a62cbca0a0c0
Justin Shilling <loveofmusic25@hotmail.com>
2023-08-31 07:58:52 -0400

    adjust about copy

--- pages/about.vue
+++ pages/about.vue
@@ -4,13 +4,13 @@
     <div class="content-wrapper">
       <div class="content-container">
         <p class="copy">
-          The Steele Rail Band is a variety band that covers music spanning
-          several decades in many different styles - such as country, southern
-          rock, alternative, classic rock and blues. Whether you feel like
-          dancing, singing along, or just enjoying the music and atmosphere,
-          we've put together a show that has you covered. We play venues of all
-          sizes including fairs/festivals, campgrounds, clubs/bars, casinos &
-          private parties.
+          The Steele Rail Band is a country-leaning variety band that covers
+          music spanning several decades in many styles. In addition to country,
+          we play southern rock, alternative, classic rock and blues. Whether
+          you feel like dancing, singing along, or just enjoying the music and
+          atmosphere, we've put together a show that has you covered. We play
+          venues of all sizes including fairs/festivals, campgrounds,
+          clubs/bars, casinos & private parties.
         </p>
         <div class="member-images">
           <div class="member-container">
